8bed562a18eef7a4e3a229d14a9b81e1b4e5f0a0
Marián Kyral <mkyral@email.cz>
2011-09-06 20:46:56 +0200

    Translation update 06.09.2011

--- refresh_files.sh
+++ refresh_files.sh
@@ -13,9 +13,12 @@ do
 
   cd "$BRANCH"
   [ -f "$GET_FILE" ] && rm "$GET_FILE" 
+  [ -f "${GET_FILE}.dos" ] && rm "${GET_FILE}.dos"
   [ -f "${PO_LANG}.po.orig" ] && rm "${PO_LANG}.po.orig"
   mv "${PO_LANG}.po" "${PO_LANG}.po.orig"
   wget --no-verbose "http://rawtherapee.googlecode.com/hg/rtdata/languages/${GET_FILE}?r=${BRANCH}" -O "$GET_FILE"
+  mv $GET_FILE ${GET_FILE}.dos
+  cat ${GET_FILE}.dos |tr -d '\r' >${GET_FILE}
   $WDIR/rtkeys2pot.sh
   msgmerge "${PO_LANG}.po.orig" rawtherapee.pot >${PO_LANG}.po
   cd - >/dev/null
