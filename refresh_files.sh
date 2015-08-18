3379095acae1f22a49845a36431c8ad41501f132
Marián Kyral <mkyral@email.cz>
2015-08-18 19:18:20 +0200

    Project moved to github, remove branch_3.0

--- refresh_files.sh
+++ refresh_files.sh
@@ -16,7 +16,8 @@ do
   [ -f "${GET_FILE}.dos" ] && rm "${GET_FILE}.dos"
   [ -f "${PO_LANG}.po.orig" ] && rm "${PO_LANG}.po.orig"
   mv "${PO_LANG}.po" "${PO_LANG}.po.orig"
-  wget --no-verbose "http://rawtherapee.googlecode.com/hg/rtdata/languages/${GET_FILE}?r=${BRANCH}" -O "$GET_FILE"
+#  wget --no-verbose "http://rawtherapee.googlecode.com/hg/rtdata/languages/${GET_FILE}?r=${BRANCH}" -O "$GET_FILE"
+  wget --no-verbose "https://raw.githubusercontent.com/Beep6581/RawTherapee/${BRANCH}/rtdata/languages/${GET_FILE}" -O "$GET_FILE"
   mv $GET_FILE ${GET_FILE}.dos
   cat ${GET_FILE}.dos |tr -d '\r' >${GET_FILE}
   $WDIR/rtkeys2pot.sh
